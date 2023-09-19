69c543228e6c2ef5d212348ea991b551b9d16971
Anchal <anchalpaswan10@gmail.com>
2023-09-19 21:49:17 +0530

    errorMiddlerware typo fixed

--- middlewares/error.js
+++ middlewares/error.js
@@ -5,7 +5,7 @@ class ErrorHandler extends Error {
     }
 }
 
-export const errorMiddlerware = (err, req, res, next) => {
+export const errorMiddleware = (err, req, res, next) => {
     err.message = err.message || "Internal server error";
     err.statusCode = err.statusCode || 500;
     return res.status(err.statusCode).json({
