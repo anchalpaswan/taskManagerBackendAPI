55b698219c733f7b1bf53f23c537b8b35c359370
Anchal <anchalpaswan10@gmail.com>
2023-11-02 17:58:47 +0530

    deleted cors and added middlerware

--- app.js
+++ app.js
@@ -17,16 +17,14 @@ config({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    credentials: true,
-}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://65437f688fec0a632c11d290--inquisitive-moonbeam-851216.netlify.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 
 
 // Using Routes
